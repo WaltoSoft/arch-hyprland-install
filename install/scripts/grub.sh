eddff21a0f762a66face0ddd16b3f5ac38c1a800
Billy Walton <billyjwalton@outlook.com>
2024-12-11 20:11:48 -0600

    Added script to install a arch-linux themed grub boot loader.

---
+++ install/scripts/grub.sh
@@ -0,0 +1,35 @@
+executeScript() {
+  local grubScriptFile=/boot/grub/grub.cfg
+  local grubConfigFile=/etc/default/grub
+  local grubThemesFolder=/usr/share/grub/themes
+
+  if isInstalledWithPacman grub && [ -f $grubScriptFile ] ; then
+    if [ ! -f "${grubConfigFile}.ahi.bkp" ] && [ ! -f "${grubScriptFile}.ahi.bkp" ]; then
+      echoText -fc $COLOR_AQUA "Grub"
+
+      echoText "Configuring Grub"
+
+      cp $grubConfigFile "${grubConfigFile}.ahi.bkp"
+      cp $grubScriptFile "${grubScriptFile}.ahi.bkp"
+
+      ensureFolder $grubThemeFolder/arch-linux
+
+      echoText "Setting grub theme"
+      tar -C $grubThemeFolder/arch-linux -xf $REPO_DIR/install/assets/arch-linux.tar
+      
+      sudo sed -i "/^GRUB_DEFAULT=/c\GRUB_DEFAULT=saved
+      /^GRUB_GFXMODE=/c\GRUB_GFXMODE=1920x1080
+      /^GRUB_TERMINAL_OUTPUT=console/c\#GRUB_TERMINAL_OUTPUT=console
+      /^GRUB_THEME=/c\GRUB_THEME=\"${grubThemesFolder}/arch_linux/theme.txt\"
+      /^#GRUB_THEME=/c\GRUB_THEME=\"${grubThemesFolder}/arch_linux/theme.txt\"
+      /^#GRUB_SAVEDEFAULT=true/c\GRUB_SAVEDEFAULT=true" $grubConfigFile
+
+      echoText "Making the new grub script file"
+      grub-mkconfig -o $grubScriptFile
+
+      echoText -c $COLOR_GREEN "Grub ocnfiguration complete!"
+    fi
+  fi
+}
+
+executeScript
--- install/scripts/main.sh
+++ install/scripts/main.sh
@@ -4,5 +4,6 @@ source $SCRIPTS_DIR/aurPackages.sh
 source $SCRIPTS_DIR/dotfiles.sh
 source $SCRIPTS_DIR/bashrc.sh
 source $SCRIPTS_DIR/sddm.sh
+source $SCRIPTS_DIR/grub.sh
 source $SCRIPTS_DIR/reboot.sh
 
