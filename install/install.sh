12c55d2fc01bfc534beeef45e0a3eac55d372ba6
Billy Walton <165430091+WaltoSoft@users.noreply.github.com>
2024-12-13 01:47:51 -0600

    Fixed another bug

--- install/install.sh
+++ install/install.sh
@@ -2,7 +2,8 @@ executeScript() {
   set -e
 
   local scriptsDir=$(dirname "$(realpath "$0")")
-  
+  scriptsDir="${scriptsDir}/scripts"
+
   source $scriptsDir/library.sh
   source $scriptsDir/validateSudo.sh
   source $scriptsDir/start.sh
