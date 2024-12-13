9fc5fc25b415f8223c53ef61a0b194057f987113
Billy Walton <165430091+WaltoSoft@users.noreply.github.com>
2024-12-13 01:48:34 -0600

    Fixed to use new file names.

--- install/install.sh
+++ install/install.sh
@@ -7,8 +7,7 @@ executeScript() {
   source $scriptsDir/library.sh
   source $scriptsDir/validateSudo.sh
   source $scriptsDir/start.sh
-  source $scriptsDir/pacmanPackages.sh
-  source $scriptsDir/aurPackages.sh
+  source $scriptsDir/packageInstall.sh
   source $scriptsDir/dotfiles.sh
   source $scriptsDir/bashrc.sh
   source $scriptsDir/sddm.sh
